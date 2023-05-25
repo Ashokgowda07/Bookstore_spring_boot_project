3c07766738a1631ea594b515902b230bc6164ddc
ASHOK KUMAR <ashokgowda8940@gmail.com>
2023-05-25 12:21:35 +0530

    Add files via upload

---
+++ BookStore/src/main/java/com/bookStore/service/MyBookListService.java
@@ -0,0 +1,34 @@
+package com.bookStore.service;
+
+import java.util.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bookStore.entity.MyBookList;
+import com.bookStore.repository.MyBookRepository;
+
+@Service
+public class MyBookListService {
+	
+	@Autowired
+	private MyBookRepository mybook;
+	
+	public void saveMyBooks(MyBookList book) {
+		mybook.save(book);
+	}
+	
+	public List<MyBookList> getAllMyBooks(){
+		return mybook.findAll();
+	}
+
+	public void deleteById(int id) {
+		mybook.deleteById(id);
+	}
+
+	/*public void deleteById(int id) {
+		// TODO Auto-generated method stub
+		
+	}*/
+
+	
+}
